067d62fc2fa7fe08ca9635644f77f9627f0396ef
derekni <dan82@cornell.com>
2022-12-04 19:08:31 -0500

    add mail icon

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,7 +1,12 @@
 import { ReactElement } from 'react';
 
+import MailIcon from './MailIcon';
+
 const Footer = (): ReactElement => (
   <div className="flex flex-1 px-28 py-12 flex-row justify-end">
+    <a href="mailto:dan82@cornell.edu" className="pr-6">
+      <MailIcon />
+    </a>
     <a href="https://www.linkedin.com/in/derek-ni/" className="h-7 w-7">
       <img src="img/linkedin.png" />
     </a>
